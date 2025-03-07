8d5308c44a23f6fb8005eeb54ef23e235abbffa3
dapa0905 <kookangmo0905@gmail.com>
2025-03-07 17:56:59 +0900

    03/07_commit2
    03/07_commit2

---
+++ my-management-mate/src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './componet/header/header.component';
+import { FooterComponent } from './componet/footer/footer.component';
+import { ItemListComponent } from './componet/item-list/item-list.component';
+import { ItemDetailComponent } from './componet/item-detail/item-detail.component';
+import { ItemFormComponent } from './componet/item-form/item-form.component';
+import { LoginComponent } from './componet/login/login.component';
+import { DashboardComponent } from './componet/dashboard/dashboard.component';
+import { NotificationsComponent } from './componet/notifications/notifications.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    ItemListComponent,
+    ItemDetailComponent,
+    ItemFormComponent,
+    LoginComponent,
+    DashboardComponent,
+    NotificationsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
