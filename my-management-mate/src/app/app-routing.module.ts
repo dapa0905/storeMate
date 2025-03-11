88db1c5ba1310eed0f2bb54774246c4a9942c26b
dapa0905 <kookangmo0905@gmail.com>
2025-03-11 17:50:18 +0900

    03/11_commit3
    03/11_commit3

--- my-management-mate/src/app/app-routing.module.ts
+++ my-management-mate/src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { ItemListComponent } from './componet/item-list/item-list.component';
 import { DashboardComponent } from './componet/dashboard/dashboard.component';
 import { ItemFormComponent } from './componet/item-form/item-form.component';
+import { ItemDetailComponent } from './componet/item-detail/item-detail.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/dashBoard', pathMatch: 'full'},
@@ -10,6 +11,7 @@ const routes: Routes = [
   { path: 'item-list', component: ItemListComponent },
   { path: 'item-form/:id', component: ItemFormComponent},
   { path: 'item-form', component: ItemFormComponent},
+  { path: 'item-detail/:id', component: ItemDetailComponent},
 
 
 ];
